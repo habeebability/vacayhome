76be61018184569320a8aa538d1c433fd5d2f03a
HABEEB AWOYEMI <awoyemi.habeeb@gmail.com>
2021-08-08 18:44:42 +0100

    fixed the scroll-to-top button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const scrollControl = document.querySelector('#scroll-top');
+let scrollFunction = () => {
+    let y = window.scrollY;
+    if (y >= 100) {
+      scrollControl.classList.remove('hide-scroll-top');
+      scrollControl.classList.add('show-scroll-top');
+  }
+
+  else {
+      scrollControl.classList.remove('show-scroll-top');
+      scrollControl.classList.add('hide-scroll-top');
+  }
+
+}
+
+window.addEventListener('scroll', scrollFunction);
+
+
 const navToggle = document.querySelector('.nav-links');
 
 const hamburger = document.querySelector('.nav-toggler');
